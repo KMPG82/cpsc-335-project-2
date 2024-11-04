9c4bcc6ea174e09ffb5c6a68987f48e95f26e2b9
Kevin <kevinponting82@gmail.com>
2024-11-03 18:18:32 -0800

    created a function that converts a schedule to minutes

--- project-2.py
+++ project-2.py
@@ -1,4 +1,25 @@
 from datetime import datetime
+def convertToMinutes(schedule, dailyAct):
+    minute_intervals=[]
+
+    clock_in = datetime.strptime(dailyAct[0], '%H:%M') #convert to datetime object
+    clock_in_total_minutes = clock_in.hour * 60 + clock_in.minute #calculate the total minutes corresponding to given time
+   
+    clock_out = datetime.strptime(dailyAct[1], '%H:%M') #convert to datetime object
+    clock_out_total_minutes = clock_out.hour * 60 + clock_out.minute #calculate the total minutes corresponding to given time
+    
+    daily_act_minutes=[clock_in_total_minutes, clock_out_total_minutes]
+    
+    for i in range(len(schedule)):
+        start_time = datetime.strptime(schedule[i][0], '%H:%M') 
+        start_total_minutes = start_time.hour * 60 + start_time.minute 
+
+        end_time = datetime.strptime(schedule[i][1], '%H:%M') 
+        end_total_minutes = end_time.hour * 60 + end_time.minute
+
+        minute_intervals.append([start_total_minutes, end_total_minutes])
+
+    return minute_intervals, daily_act_minutes
 
 def findFreeTimes(schedule, dailyAct): #function that finds intervals of free times in a schedule
     free_time=[] #initilizae free time list
@@ -72,7 +93,15 @@ duration_of_meeting = 30
 # Sample output
 # [[’10:30’, ’12:00’], [’15:00’, ’16:00’], [’18:00’, ’18:30’]]
 
-person1_Gaps=findFreeTimes(person1_Schedule, person1_DailyAct)
-person2_Gaps=findFreeTimes(person2_Schedule, person2_DailyAct)
+#person1_Gaps=findFreeTimes(person1_Schedule, person1_DailyAct)
+#person2_Gaps=findFreeTimes(person2_Schedule, person2_DailyAct)
 
-print('valid intervals: ', findValidTimes(person1_Gaps, person2_Gaps, duration_of_meeting))
+#print('valid intervals: ', findValidTimes(person1_Gaps, person2_Gaps, duration_of_meeting))
+
+schedule_in_min, daily_act_in_min=convertToMinutes(person1_Schedule, person1_DailyAct)
+print('schedule in minutes: ', schedule_in_min)
+print('daily act in min: ', daily_act_in_min)
+
+schedule_in_min, daily_act_in_min=convertToMinutes(person2_Schedule, person2_DailyAct)
+print('schedule in minutes: ', schedule_in_min)
+print('daily act in min: ', daily_act_in_min)
